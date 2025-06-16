77f9898c5ae019ca1afeda1044e2aad37e887285
Stormy31 <antonijoelmoreno@gmail.com>
2025-06-16 09:39:16 -0500

    E10.cpp

--- E10.cpp
+++ E10.cpp
@@ -7,25 +7,25 @@ int main() {
     cout << "Introduce el número de términos de la serie Fibonacci que deseas imprimir: ";
     cin >> n;
 
-    // Verificamos que n sea un número positivo
+
     if (n <= 0) {
         cout << "Por favor, introduce un número entero positivo." << endl;
     } else {
-        long long a = 0; // Primer término
-        long long b = 1; // Segundo término
+        long long a = 0; 
+        long long b = 1; 
 
         cout << "Serie Fibonacci: " << endl;
 
         for (int i = 1; i <= n; ++i) {
             cout << a << " "; 
 
-            // Calculamos el siguiente término
+            
             long long siguiente = a + b;
-            a = b; // Actualizamos a
-            b = siguiente; // Actualizamos b
+            a = b; 
+            b = siguiente; 
         }
 
-        cout << endl; // 
+        cout << endl; 
     }
 
     return 0;
