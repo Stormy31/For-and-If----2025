6dfdb65d3289a7403548f6c428fbb3de2bda9823
Stormy31 <antonijoelmoreno@gmail.com>
2025-06-16 09:39:44 -0500

    E9.cpp

--- E9.cpp
+++ E9.cpp
@@ -9,13 +9,13 @@ int main() {
     cout << "Introduce un número entero: ";
     cin >> numero;
 
-    // Convertimos el número a string
+    
     string numeroStr = to_string(numero);
 
-    // Sumamos los dígitos usando un bucle for
+    
     for (size_t i = 0; i < numeroStr.length(); ++i) {
-        // Convertimos cada carácter a su valor numérico y lo sumamos
-        suma Dígitos += numeroStr[i] - '0'; // Restamos '0' para convertir char a int
+        
+        suma Dígitos += numeroStr[i] - '0'; 
     }
 
     cout << "La suma de los dígitos de " << numero << " es " << suma Dígitos << endl;
